ca7f69e332fa3fc4e8acb6b85e8c43130fe3f635
kuk329 <kuk329@cau.ac.kr>
2022-01-24 23:08:17 +0900

    RecentQuestionAllActivity -> MyQuestionAllActivity

--- app/src/main/java/com/example/mumulcom/HomeFragment.kt
+++ app/src/main/java/com/example/mumulcom/HomeFragment.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mumulcom.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
-    private var myQuestions = ArrayList<Question>()
+    private var recentQuestions = ArrayList<Question>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,19 +21,159 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        for(i in 0..3){
+            recentQuestions.add(Question("$i person","2022-01-($i)","I have a question"))
+        }
 
 
+//        val recentQuestionAdapter = RecentQuestionAdapter(recentQuestions)
+//        binding.recentQuestionVp.adapter = recentQuestionAdapter
+//        binding.recentQuestionVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+//        binding.homeIndicator.setViewPager(binding.recentQuestionVp)
+
+
+
+
+
+
+
+
+
+
+
+        // 나의 최근 질문 전체보기
         binding.watchRecentQuestionAllTv.setOnClickListener {
-            // Activity 변경
-            startActivity(Intent(context,RecentQuestionAllActivity::class.java))
+            startActivity(Intent(context,MyQuestionAllActivity::class.java))
         }
 
-        binding.appAllTv.setOnClickListener {
-            //앱  전체 게시판 클릭시
-            startActivity(Intent(context,QuestionBoardActivity::class.java))
-        }
+        initCategoryButton()
 
         return binding.root
+    }
+
+    private fun initCategoryButton() {
+        // ------------          앱       -------------------------
+        binding.appAllTv.setOnClickListener {
+            //앱  전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","앱")
+            startActivity(intent)
+        }
+        binding.androidTv.setOnClickListener {
+            // 안드로이드 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","안드로이드")
+            startActivity(intent)
+        }
+        binding.iosTV.setOnClickListener {
+            // IOS 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","ios")
+            startActivity(intent)
+        }
+
+
+
+        // --------------         웹       -----------------------------
+
+        binding.webAllTv.setOnClickListener {
+            //웹  전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","웹")
+            startActivity(intent)
+        }
+        binding.htmlTv.setOnClickListener {
+            //html  전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","HTML")
+            startActivity(intent)
+        }
+        binding.cssTv.setOnClickListener {
+            //CSS  전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","CSS")
+            startActivity(intent)
+        }
+        binding.reactTv.setOnClickListener {
+            // React 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","React")
+            startActivity(intent)
+        }
+
+
+        // --------------         서버      -----------------------------
+
+        binding.serverAllTv.setOnClickListener {
+            //서버  전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","서버")
+            startActivity(intent)
+        }
+
+
+        binding.nodeTv.setOnClickListener {
+            // Node Js 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","Node Js")
+            startActivity(intent)
+        }
+
+        binding.springTv.setOnClickListener {
+            //  SPRING   게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","Spring")
+            startActivity(intent)
+        }
+        // --------------         프로그래밍 언어      -----------------------------
+
+        binding.languageAllTv.setOnClickListener {
+            //프로그래밍 언어 전체 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","프로그래밍 언어")
+            startActivity(intent)
+        }
+        binding.cTv.setOnClickListener {
+            // C 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","프로그래밍 언어")
+            startActivity(intent)
+        }
+        binding.javaTv.setOnClickListener {
+            // Java 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","Java")
+            startActivity(intent)
+        }
+        binding.jSTv.setOnClickListener {
+            // Js 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","JavaScript")
+            startActivity(intent)
+        }
+        binding.pythonTv.setOnClickListener {
+            // Python 게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","Python")
+            startActivity(intent)
+        }
+
+        binding.cPlusTv.setOnClickListener {
+            // C++  게시판 클릭시
+            val intent = Intent(context,QuestionBoardActivity::class.java,)
+            intent.putExtra("category","C++")
+            startActivity(intent)
+        }
+        binding.etcAllTv.setOnClickListener {
+            // 기타 게시판
+            val intent = Intent(context,QuestionBoardActivity::class.java)
+            intent.putExtra("category","기타")
+            startActivity(intent)
+        }
+
+
+
+
     }
 
 
--- app/src/main/java/com/example/mumulcom/QuestionBoardActivity.kt
+++ app/src/main/java/com/example/mumulcom/QuestionBoardActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mumulcom
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mumulcom.databinding.ActivityQuestionBoardBinding
@@ -8,10 +9,107 @@ import com.example.mumulcom.databinding.ActivityQuestionBoardBinding
 // Frame62
 class QuestionBoardActivity : AppCompatActivity() {
     private lateinit var binding : ActivityQuestionBoardBinding
+    private var codingQuestionCheck : Boolean = true // default 값
+    private var conceptQuestionCheck : Boolean = false // default 값
+    private var recentQuestionCheck : Boolean = true
+    private var hotQuestionCheck : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        initCodingOrConceptQuestionButton() // 코딩 질문 & 개념 질문 버튼 초기화
+        initRecentOrHotQuestionTextButton() // 최신순 & 핫한순 버튼 초기화
+
+        val intent = intent
+        val title = intent.getStringExtra("category")
+        binding.categoryNameTv.text = title
+
+
+
+        // 상단 뒤로가기 버튼
+        binding.backIv.setOnClickListener {
+            finish()
+        }
+
+        // 코딩 질문 버튼
+        binding.codingQuestionIv.setOnClickListener {
+            codingQuestionCheck = true
+            conceptQuestionCheck = false
+            initCodingOrConceptQuestionButton()
+        }
+
+
+        // 개념 질문 버튼
+        binding.conceptQuestionIv.setOnClickListener {
+            conceptQuestionCheck= true
+            codingQuestionCheck = false
+            initCodingOrConceptQuestionButton()
+        }
+
+        // 최신순
+        binding.sortRecentTv.setOnClickListener {
+            recentQuestionCheck = true
+            hotQuestionCheck = false
+            initRecentOrHotQuestionTextButton()
+
+        }
+
+        //핫한순
+        binding.sortHotTv.setOnClickListener {
+            hotQuestionCheck = true
+            recentQuestionCheck = false
+            initRecentOrHotQuestionTextButton()
+
+        }
+    }// end of onCreate
+
+
+
+
+    private fun initRecentOrHotQuestionTextButton() {
+        if(recentQuestionCheck){
+            binding.sortRecentTv.setTextColor(Color.parseColor("#474A57"))
+            binding.sortRecentTv.isEnabled = false
+            // 최근 질문순으로 보여줌
+        }else{
+            binding.sortRecentTv.setTextColor(Color.parseColor("#C4C4C4"))
+            binding.sortRecentTv.isEnabled = true
+        }
+
+        if(hotQuestionCheck){
+            binding.sortHotTv.setTextColor(Color.parseColor("#474A57"))
+            binding.sortHotTv.isEnabled = false
+            // 인기 질문 순으로 보여줌
+        }else{
+            binding.sortHotTv.setTextColor(Color.parseColor("#C4C4C4"))
+            binding.sortHotTv.isEnabled = true
+        }
+
+    }
+
+    fun initCodingOrConceptQuestionButton(){
+
+        if(codingQuestionCheck){
+            binding.codingQuestionIv.setImageResource(R.drawable.coding_question_check_img)
+            binding.codingQuestionIv.isEnabled = false // 이미 선택되었으면 선택 못함.
+            // 코딩 질문만 보여줌
+        }else{
+            binding.codingQuestionIv.setImageResource(R.drawable.coding_question_img)
+            binding.codingQuestionIv.isEnabled = true
+        }
+
+        if(conceptQuestionCheck){
+            binding.conceptQuestionIv.setImageResource(R.drawable.concept_question_check_img)
+            binding.conceptQuestionIv.isEnabled = false // 이미 선택되었으면 선택 못함.
+            // 개념 질문만 보여줌
+
+        }else{
+            binding.conceptQuestionIv.setImageResource(R.drawable.concept_question_img)
+            binding.conceptQuestionIv.isEnabled = true
+        }
+
     }
 
 
